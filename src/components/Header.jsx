73b2d501a932a041602ffb151be75a8ce45159bf
Esther Diaz <ed.alum.daw@gmail.com>
2026-01-28 10:11:11 +0100

    correcciones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import Busqueda from './Busqueda';
 import Listado from './Listado';
 import Filtrado from './Filtrado';
+import logo from '../img/Logo.png';
 
 const Header = () => {
 
     return (
         <header>
+          <div className="logotipo">
+            <h2>TransiPort</h2>
+            <img src={logo} alt="logotipo transiport" />
+          </div>
           <h1 className="titulo">Listado de buques</h1>
           <div className="filtradoBusqueda">
             <Filtrado />
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -31,17 +31,24 @@ const Listado = () => {
                 </div>
             </div>
             <div>
-                <button className="botonDetalles">Detalles
-                    <i className="fa-regular fa-note-sticky" style={{fontSize: '1.5em'}}></i>
+                <button className="boton botonDetalles">
+                    Detalles
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 256 256"><path d="M88,96a8,8,0,0,1,8-8h64a8,8,0,0,1,0,16H96A8,8,0,0,1,88,96Zm8,40h64a8,8,0,0,0,0-16H96a8,8,0,0,0,0,16Zm32,16H96a8,8,0,0,0,0,16h32a8,8,0,0,0,0-16ZM224,48V156.69A15.86,15.86,0,0,1,219.31,168L168,219.31A15.86,15.86,0,0,1,156.69,224H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32H208A16,16,0,0,1,224,48ZM48,208H152V160a8,8,0,0,1,8-8h48V48H48Zm120-40v28.7L196.69,168Z"></path></svg>
                 </button>
             </div>
         </div>
       ))}
     </div>
 
-    <div className='botonesAñadir'>
-        <button type="submit"><i class="fa-light fa-plus"></i> Añadir buque</button>
-        <button type="submit"><i class="fa-solid fa-minus"></i> Eliminar buque</button>
+    <div className="botonesAnyadir">
+        <button className="boton botonAnyadir" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path></svg> 
+            <span>Añadir buque</span>
+        </button>
+        <button className="boton botonAnyadir" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128Z"></path></svg> 
+            Eliminar buque
+        </button>
     </div>
     </div>
   )
