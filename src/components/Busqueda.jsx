c51d21d266b46e0e5a25a3b0149134cb79fbda70
esther <estdiasor@alu.edu.gva.es>
2026-02-02 11:48:45 +0100

    modificacion busqueda

--- src/components/Busqueda.jsx
+++ src/components/Busqueda.jsx
@@ -1,21 +1,13 @@
-const Busqueda = forwardRef((props, ref) => {
-
-  useImperativeHandle(ref, () => ({
-
-    realizarBusqueda() {
-      
-    }
-
-  }));
+const Busqueda = () => {
 
   return (
     <div className="flex bg-[#DFECF5] rounded-[5px] items-center mr-25.5">
       <div className="bg-[#5F84A2] w-8 h-full flex justify-center items-center rounded-[5px] text-white">
         <i className="fa-solid fa-magnifying-glass"></i>
       </div>
-      <input onChange={realizarBusqueda} className="bg-[#DFECF5] rounded-[5px] border-none outline-none pl-2 w-full" type="text" />
+      <input className="bg-[#DFECF5] rounded-[5px] border-none outline-none pl-2 w-full" type="text" />
     </div>
   )
-});
+};
 
 export default Busqueda;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import Listado from './Listado';
 import Filtrado from './Filtrado';
 import Detalles from './Detalles';
 import logo from '../img/Logo.png';
-import data from '../data/db.json';
 import { useEffect, useState } from 'react';
 import buqueService from '../services/buques'
 
@@ -20,8 +19,6 @@ const Header = () => {
       })
   }, [buques])
 
-  const busquedaRef = useRef();
-
     return (
         <header className="relative w-[90%] h-dvh pt-6 pr-6 p-2 pl-7.5 bg-white ml-7.5">
           <div className="w-full relative z-50 flex justify-end items-center text-[#2A5677] gap-4">
@@ -32,11 +29,11 @@ const Header = () => {
           <div className="w-full pt-5 flex justify-between">
             <Filtrado />
             {buqueSeleccionado === null && (
-              <Busqueda ref={busquedaRef}/>
+              <Busqueda />
             )}
           </div>
           <div className={`${buqueSeleccionado ? 'w-[53%]' : 'w-full'}`}>
-            <Listado data={data} onSelect={setBuqueSeleccionado} seleccionado={buqueSeleccionado ? true : false}/>
+            <Listado data={buques} onSelect={setBuqueSeleccionado} seleccionado={buqueSeleccionado ? true : false}/>
           </div>
             {buqueSeleccionado !== null && (
               <div className="absolute top-0 right-0 bg-[#B7D0E1] h-full w-[45%] rounded-l-[50px] pt-[7%] pl-[5%] pr-[5%]">
