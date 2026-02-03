c10e18c6f832434a48f2eb9052596d18db80dcd0
esther <estdiasor@alu.edu.gva.es>
2026-02-03 10:34:05 +0100

    todo correcto

--- src/components/FormAnyadir.jsx
+++ src/components/FormAnyadir.jsx
@@ -5,42 +5,53 @@ const FormAnyadir = () => {
     const columnStyle = "flex flex-wrap w-[50%]";
 
     return (
-        <div className="w-180 h-180 pl-18 pr-18 pt-13 bg-[#B7D0E1] rounded-[50px]">
-            <h1 className="text-3xl font-bold text-[#2A5677] pb-8">Añadir buque</h1>
-            <form className="text-[#2A5677]" action="">
-                <div className={lineStyle}>
-                    <div className={columnStyle}>
-                        <label htmlFor="id_buque">ID buque</label>
-                        <input className={`${inputStyle} mt-3`} type="text" id="id_buque" name="id" value="B-" readOnly/>
-                    </div>
-                    <div className={columnStyle}>
-                        <label htmlFor="estado_buque">Estado</label>
-                        <select className={`${inputStyle} mt-3 p-1.5`} name="estado" id="estado_buque">
-                            <option className="p-3" value="espera">En espera</option>
-                            <option className="p-3" value="atracado">Atracado</option>
-                            <option className="p-3" value="inactivo">Inactivo</option>
-                        </select>
-                    </div>
+        <div>
+            <div className="flex justify-center mt-7">
+                <div className="w-180 h-180 pl-18 pr-18 pt-13 bg-[#B7D0E1] rounded-[50px]">
+                    <h1 className="text-3xl font-bold text-[#2A5677] pb-8">Añadir buque</h1>
+                    <form className="text-[#2A5677]" action="">
+                        <div className={lineStyle}>
+                            <div className={columnStyle}>
+                                <label htmlFor="id_buque">ID buque</label>
+                                <input className={`${inputStyle} mt-3`} type="text" id="id_buque" name="id" value="B-" readOnly/>
+                            </div>
+                            <div className={columnStyle}>
+                                <label htmlFor="estado_buque">Estado</label>
+                                <select className={`${inputStyle} mt-3 p-1.5`} name="estado" id="estado_buque">
+                                    <option className="p-3" value="espera">En espera</option>
+                                    <option className="p-3" value="atracado">Atracado</option>
+                                    <option className="p-3" value="inactivo">Inactivo</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className={`${lineStyle} mt-8 mb-8`}>
+                            <div className={columnStyle}>
+                                <label htmlFor="nombre_buque">Nombre</label>
+                                <input className={`${inputStyle} mt-3`} type="text" id="nombre_buque" name="nombre" value="" placeholder="La Perla Negra"/>
+                            </div>
+                            <div className={columnStyle}>
+                                <label htmlFor="tipo_buque">Tipo</label>
+                                <input className={`${inputStyle} mt-3`} type="text" id="tipo_buque" name="tipo" value="" placeholder="Portacontenedores"/>
+                            </div>
+                        </div>
+                        <label htmlFor="capacidad_buque">Capacidad</label>
+                        <input className={`${inputStyle} mt-3`} type="number" id="capacidad_buque" name="capacidad" value="" placeholder="50"/>
+                        <div className="mt-8">
+                            <label htmlFor="observaciones_buque">Observaciones</label>
+                            <textarea className={`${inputStyle} mt-3`} name="observaciones" id="observaciones_buque" value="" rows="4"></textarea>
+                        </div>
+                        <div className="mt-6 flex justify-center">
+                            <button className="bg-[#5F84A2] text-white font-bold gap-2 pt-2 pb-2 pr-4 pl-4 rounded-[5px] flex items-center justify-around text-lg hover:bg-[#DFECF5] hover:text-[#5F84A2] hover:border-3 hover:border-[#5F84A2]">
+                                <span>Crear</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path></svg>
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div className={`${lineStyle} mt-8 mb-8`}>
-                    <div className={columnStyle}>
-                        <label htmlFor="nombre_buque">Nombre</label>
-                        <input className={`${inputStyle} mt-3`} type="text" id="nombre_buque" name="nombre" value="" placeholder="La Perla Negra"/>
-                    </div>
-                    <div className={columnStyle}>
-                        <label htmlFor="tipo_buque">Tipo</label>
-                        <input className={`${inputStyle} mt-3`} type="text" id="tipo_buque" name="tipo" value="" placeholder="Portacontenedores"/>
-                    </div>
-                </div>
-                <label htmlFor="capacidad_buque">Capacidad</label>
-                <input className={`${inputStyle} mt-3`} type="number" id="capacidad_buque" name="capacidad" value="" placeholder="50"/>
-                <div className="mt-8">
-                    <label htmlFor="observaciones_buque">Observaciones</label>
-                    <textarea className={`${inputStyle} mt-3`} name="observaciones" id="observaciones_buque" value="" rows="4"></textarea>
-                </div>
-                
-            </form>
+            </div>
+            
         </div>
+        
     )
 
 }
