0a92c8cfeb28f865a1fb6ca38bfaf7bd1d529e35
esther <estdiasor@alu.edu.gva.es>
2026-02-03 10:37:18 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,12 @@ import Botones from './Botones';
 import { useState, useEffect } from 'react';
 import FormAnyadir from './FormAnyadir';
 import buqueService from '../services/buques'
-import axios from 'axios';
 
 const Header = () => {
 
   const [buques, setBuques] = useState([]);
   const [buqueSeleccionado, setBuqueSeleccionado] = useState(null);
   const [crearBuque, setCrearBuque] = useState(false);
-
   
   useEffect(()=>{
     buqueService
@@ -27,9 +25,18 @@ const Header = () => {
 
     return (
         <header className="relative w-[90%] h-dvh pt-6 pr-6 p-2 pl-7.5 bg-white ml-7.5">
-          <div className="w-full relative z-50 flex justify-end items-center text-[#2A5677] gap-4">
-            <h1 className="font-bold text-3xl">TransitPort</h1>
-            <img src={logo} alt="logotipo transitport" className="w-25 h-25"/>
+          <div className="w-full flex">
+            <div className="w-[50%]">
+                {crearBuque && (
+                  <button onClick={() => setCrearBuque(false)} className="bg-[#5F84A2] text-white mt-3 rounded-[5px] w-12 h-12 flex justify-center items-center hover:bg-[#DFECF5] hover:text-[#5F84A2] hover:border-3 hover:border-[#5F84A2]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path></svg>
+                  </button>
+                )}
+            </div>
+            <div className="w-[50%] relative z-50 flex justify-end items-center text-[#2A5677] gap-4">
+              <h1 className="font-bold text-3xl">TransitPort</h1>
+              <img src={logo} alt="logotipo transitport" className="w-25 h-25"/>
+            </div>
           </div>
           {!crearBuque && (
             <>
